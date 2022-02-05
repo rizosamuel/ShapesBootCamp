1f0ef556e2da08d325330bf77ef9658be654fa64
Rijo <rizosamuel1995@gmail.com>
2022-02-05 21:09:56 +0530

    Initial Commit

---
+++ ShapesBootCamp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShapesBootCamp
+//
+//  Created by Rijo Samuel on 05/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
